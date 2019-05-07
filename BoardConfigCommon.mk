f17ccd82472d9ec6614b9fbc31b6542afa7c4095
Tao Bao <tbao@google.com>
2019-05-06 22:42:02 -0700

    sdm845-common: cleanup: AB_OTA_UPDATER is BoardConfig variable.
    Bug: 130433003
    Test: TreeHugger
    Change-Id: I70c7a884df45781e8b14339edd02985ba3de02fd

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -68,6 +68,16 @@ TARGET_VENDOR_PROP += $(COMMON_PATH)/vendor.prop
 # Treble
 BOARD_VNDK_VERSION := current
 
+# A/B
+AB_OTA_UPDATER := true
+
+AB_OTA_PARTITIONS += \
+    boot \
+    dtbo \
+    system \
+    vendor \
+    vbmeta
+
 # ANT+
 BOARD_ANT_WIRELESS_DEVICE := "qualcomm-hidl"
 
