2085f098c5c1bb7c67e812a57bd71bc21a5ca983
Anirudh Gupta <anirudhgupta109@gmail.com>
2020-01-01 13:42:23 +0000

    sdm845-common: DeviceSettings: Update display modes for OOS 10
    Change-Id: Iedfa24b0c4c6a8304839ff3ddf25119a6b44bf62
    Signed-off-by: Anirudh Gupta <anirudhgupta109@gmail.com>

--- DeviceSettings/src/org/lineageos/device/DeviceSettings/ModeSwitch/DCIModeSwitch.java
+++ DeviceSettings/src/org/lineageos/device/DeviceSettings/ModeSwitch/DCIModeSwitch.java
@@ -26,7 +26,7 @@ import androidx.preference.PreferenceManager;
 
 public class DCIModeSwitch implements OnPreferenceChangeListener {
 
-    private static final String FILE = "/sys/devices/platform/soc/ae00000.qcom,mdss_mdp/drm/card0/card0-DSI-1/DCI_P3";
+    private static final String FILE = "/sys/devices/platform/soc/ae00000.qcom,mdss_mdp/drm/card0/card0-DSI-1/native_display_p3_mode";
 
     public static String getFile() {
         if (Utils.fileWritable(FILE)) {
--- DeviceSettings/src/org/lineageos/device/DeviceSettings/ModeSwitch/WideModeSwitch.java
+++ DeviceSettings/src/org/lineageos/device/DeviceSettings/ModeSwitch/WideModeSwitch.java
@@ -1,5 +1,5 @@
 /*
-* Copyright (C) 2018 The OmniROM Project
+* Copyright (C) 2017 The OmniROM Project
 *
 * This program is free software: you can redistribute it and/or modify
 * it under the terms of the GNU General Public License as published by
@@ -24,9 +24,9 @@ import androidx.preference.Preference;
 import androidx.preference.Preference.OnPreferenceChangeListener;
 import androidx.preference.PreferenceManager;
 
-public class OnePlusModeSwitch implements OnPreferenceChangeListener {
+public class WideModeSwitch implements OnPreferenceChangeListener {
 
-    private static final String FILE = "/sys/devices/platform/soc/ae00000.qcom,mdss_mdp/drm/card0/card0-DSI-1/oneplus_mode";
+    private static final String FILE = "/sys/devices/platform/soc/ae00000.qcom,mdss_mdp/drm/card0/card0-DSI-1/native_display_wide_color_mode";
 
     public static String getFile() {
         if (Utils.fileWritable(FILE)) {
