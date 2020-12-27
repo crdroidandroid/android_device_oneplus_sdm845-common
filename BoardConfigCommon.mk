9d8967602f89ce0b0a564cab3e27258bb396e477
Terminator-J <34873300+Terminator-J@users.noreply.github.com>
2020-12-27 20:57:58 +0000

    Re-add kernel defconfig We kinda need that. Whoopsie-doodle.
    Change-Id: Ia7b5874cc62eeb8dacb9fd721b4c2c34eaad0462

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -54,6 +54,7 @@ TARGET_KERNEL_ARCH := arm64
 TARGET_KERNEL_SOURCE := kernel/oneplus/sdm845
 TARGET_KERNEL_CLANG_COMPILE := true
 TARGET_USE_LATEST_CLANG := true
+TARGET_KERNEL_CONFIG := enchilada_defconfig
 #BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 
 # Platform
