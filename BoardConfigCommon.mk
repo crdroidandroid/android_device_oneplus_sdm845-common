karin722/device_oneplus_sdm845-common

sdm845-common: move defconfig to common
Change-Id: If0571467f0a720d0d96303149975c998bca623d3

BoardConfigCommon.mk
@@ -46,6 +46,7 @@ BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
 BOARD_KERNEL_PAGESIZE := 4096
 BOARD_KERNEL_SEPARATED_DTBO := true
 NEED_KERNEL_MODULE_SYSTEM := true
+TARGET_KERNEL_CONFIG := radioactive_defconfig
 TARGET_KERNEL_CLANG_COMPILE := true
 TARGET_KERNEL_CLANG_VERSION := 6194136
 TARGET_KERNEL_ARCH := arm64

crdroidandroid/android_device_oneplus_sdm845-common

BoardConfigCommon.mk
@@ -50,6 +50,7 @@ BOARD_KERNEL_PAGESIZE := 4096
 BOARD_KERNEL_SEPARATED_DTBO := true
 BOARD_MKBOOTIMG_ARGS += --header_version $(BOARD_BOOT_HEADER_VERSION)
 NEED_KERNEL_MODULE_SYSTEM := true
+TARGET_KERNEL_CONFIG := crdroid_defconfig
 TARGET_KERNEL_ARCH := arm64
 TARGET_KERNEL_SOURCE := kernel/oneplus/sdm845
 TARGET_KERNEL_CLANG_COMPILE := true