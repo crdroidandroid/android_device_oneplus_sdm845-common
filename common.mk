34ca18cb236c8ea99b0b1e6beedbdb4b3e40fc72
Terminator-J <34873300+Terminator-J@users.noreply.github.com>
2021-05-01 09:08:16 -0700

    sdm845-common: DeviceSettings: Implement DeviceSettings
    Based on sm8150-common by Lup Gabriel <gwolf2u.website@gmail.com>
    Starting from https://github.com/crdroidandroid/android_device_oneplus_sm8150-common/commit/ca8d2c2fde06eebc63f481855110012b51b9a55d

--- common.mk
+++ common.mk
@@ -99,6 +99,10 @@ PRODUCT_PACKAGES += \
     init.recovery.qcom.rc \
     ueventd.qcom.rc
 
+# Device Settings
+PRODUCT_PACKAGES += \
+    DeviceSettings
+
 # Display
 PRODUCT_PACKAGES += \
     libdisplayconfig \
@@ -193,7 +197,6 @@ PRODUCT_PACKAGES += \
 
 # tri-state-key
 PRODUCT_PACKAGES += \
-    KeyHandler \
     tri-state-key_daemon
 
 # Trust HAL
