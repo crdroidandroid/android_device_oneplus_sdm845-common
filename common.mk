5dddba3c21bfc8f0beaf9b446ee44e765a918b12
Terminator-J <terminator_j@hotbutteredmonkey.com>
2021-07-14 11:33:51 -0700

    sdm845-common: packages: Remove Snap camera app
    Nuke Qualcomm Snapdragon camera, since Lineage
    dropped it already, and add "remove-packages"
    metapackage to allow overriding other packages.

--- common.mk
+++ common.mk
@@ -165,7 +165,6 @@ PRODUCT_PACKAGES_DEBUG += \
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service \
-    Snap \
     vendor.qti.hardware.camera.device@1.0.vendor
 
 # Common init scripts
@@ -370,6 +369,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/qti_whitelist.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/qti_whitelist.xml \
     $(LOCAL_PATH)/configs/privapp-permissions-qti.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/privapp-permissions-qti.xml
+
+# Remove unwanted packages
+PRODUCT_PACKAGES += \
+    RemovePackages
 
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
