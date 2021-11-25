e918bcc71fc6d3f3b28f13ecbceb026dba584345
DennySPb <dennyspb@gmail.com>
2021-11-25 10:28:39 +0300

    sdm845-common: cleanup: Fix AVB flags
        * must be flags
    
    Signed-off-by: DennySPb <dennyspb@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -122,7 +122,7 @@ BOARD_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/vendor
 
 # Verified Boot
 BOARD_AVB_ENABLE := true
-BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flag 2
+BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flags 2
 
 # Inherit from the proprietary version
 -include vendor/oneplus/sdm845-common/BoardConfigVendor.mk
