06f0c7a8af6428a0d682702b725b57e1546f7f32
Terminator-J <terminator_j@hotbutteredmonkey.com>
2021-12-09 16:03:04 -0800

    sdm845-common: cleanup: Add missing permissions

--- common.mk
+++ common.mk
@@ -186,6 +186,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_BOOT_JARS += \
     telephony-ext
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/privapp-permissions-qti.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/privapp-permissions-qti.xml
+
 # Touch
 PRODUCT_PACKAGES += \
     lineage.touch@1.0-service.oneplus
