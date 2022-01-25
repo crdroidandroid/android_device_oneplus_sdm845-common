bfd1b5cefdfef2c688e541ac5710883030f83492
Josh Wu <joshwu@google.com>
2022-01-24 22:56:14 -0800

    sdm845-common: bluetooth: Switch BtAudio to AIDL
    Test: m
    Bug: 203490261
    Change-Id: I4b8ac40d5ad410b89eb6a0573d740d86471ecfc7
    Merged-In: I4b8ac40d5ad410b89eb6a0573d740d86471ecfc7

--- common.mk
+++ common.mk
@@ -98,7 +98,6 @@ PRODUCT_PACKAGES += \
     android.hardware.audio@6.0-impl \
     android.hardware.audio.effect@6.0-impl \
     android.hardware.audio.service \
-    android.hardware.bluetooth.audio@2.0-impl \
     android.hardware.soundtrigger@2.1-impl \
     audio.bluetooth.default \
     audio.primary.sdm845 \
@@ -144,6 +143,7 @@ PRODUCT_COPY_FILES += \
 # Bluetooth
 PRODUCT_PACKAGES += \
     android.hardware.bluetooth@1.0.vendor \
+    android.hardware.bluetooth.audio-impl \
     liba2dpoffload \
     vendor.qti.hardware.bluetooth_audio@2.1.vendor \
     vendor.qti.hardware.btconfigstore@1.0.vendor \
