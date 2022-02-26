11b510633ec44054bcaeb837ed7000c16973f9e5
Terminator-J <terminator_j@hotbutteredmonkey.com>
2022-02-26 00:50:30 -0800

    sdm845-common: tri-state-key: Switch to vendor
    Switching to crdroid hardware/oneplus components,
    where we still have tri-state-key daemon and it's
    building in vendor now, since new kotlin-based
    KeyHandler is 1) kinda crap, and 2) doesn't support
    broadcasts for OOS-style alert slider notification
    bubbles, which is feature we like.
    
    This reverts commit c3e4dd2253e00206f961a901816b34594ba5c405,
    "sdm845-common: Adjust for recent tri state key changes", and
    partially reverts 413e9eaba75d627ff2c205a591ce61b5ab74b091,
    "sdm845-common: Switch to hardware/oneplus components".

--- common.mk
+++ common.mk
@@ -381,7 +381,8 @@ $(call soong_config_set,ONEPLUS_LINEAGE_TOUCH_HAL,INCLUDE_DIR,$(LOCAL_PATH)/touc
 
 # tri-state-key
 PRODUCT_PACKAGES += \
-    KeyHandler
+    KeyHandler \
+    tri-state-key_daemon.vendor
 
 # Update engine
 PRODUCT_PACKAGES += \
