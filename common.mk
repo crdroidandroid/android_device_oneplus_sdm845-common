f427d43007089c070d72278e020f76490ecb9da7
jabashque <jabashque@gmail.com>
2022-04-10 15:56:24 -0700

    sdm845-common: Retrofit dynamic partitions
    Use /system, /odm, and /vendor as the backing partitions for the super
    image.
    
    Change-Id: Ibfb6c69ce539c966fcd441da2b0d2da3bec93a54
    
    [Terminator-J]
    Don't make system_ext and product individual partitions,
    makes it harder to figure out how much free space to
    reserve for Google Apps later.

--- common.mk
+++ common.mk
@@ -301,6 +301,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     libvndfwk_detect_jni.qti.vendor # Needed by CNE app
 
+# Retrofit Dynamic Partitions
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
+PRODUCT_RETROFIT_DYNAMIC_PARTITIONS := true
+
 # RIL
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/qti_whitelist.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/qti_whitelist.xml \
