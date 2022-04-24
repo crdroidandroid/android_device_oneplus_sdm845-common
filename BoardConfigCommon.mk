b83fa41f3cd17b0803032e30c365a6394a2f4c57
Edwin Moquete <edwinmmoquete@gmail.com>
2022-04-24 19:33:22 +0000

    sdm845-common: Move DT2W handling to powerhint
    Change-Id: Icd035962d289996feb76ddeaf02bf1843bd9d64a

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -150,9 +150,6 @@ BOARD_FLASH_BLOCK_SIZE := 262144 # (BOARD_KERNEL_PAGESIZE * 64)
 TARGET_COPY_OUT_SYSTEM := system
 TARGET_COPY_OUT_VENDOR := vendor
 
-# Power
-TARGET_TAP_TO_WAKE_NODE := "/proc/touchpanel/double_tap_enable"
-
 # Recovery
 BOARD_USES_RECOVERY_AS_BOOT := true
 TARGET_NO_RECOVERY := true
