65d0998c83529e38f918c5fae8496ebf48559ea1
Terminator-J <terminator_j@hotbutteredmonkey.com>
2022-05-05 23:09:25 -0700

    sdm845-common: overlay: Enable crDroid features & customization
    Features & UI customization overlays from
    https://github.com/crdroidandroid/android_frameworks_base/blob/12.0/core/res/res/values/cr_config.xml
    and
    https://github.com/crdroidandroid/android_frameworks_base/blob/12.0/packages/SystemUI/res/values/cr_config.xml
    ...among other things. :)
    
    - Dash charging
    - Tri state switch (alert slider) & notification bubbles
    - Quick Tap options
    - Smart Pixels overlay
    - Burn-in protection
    - Disable assist hint on lockscreen
    - Volume panel on left side
    - Sensor-based doze customization
    - Nuke LineageOS pocketmode (we have one ROM-side)
    - Nuke LineageOS doze (we have one ROM-side)

--- common.mk
+++ common.mk
@@ -206,10 +206,6 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.display.mapperextensions@1.0.vendor \
     vendor.oneplus.hardware.display@1.0.vendor
 
-# Doze
-PRODUCT_PACKAGES += \
-    OnePlusDoze
-
 # DRM
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.3.vendor \
@@ -351,6 +347,9 @@ PRODUCT_PACKAGES += \
     libjson \
     libqti_vndfwk_detect \
     libqti_vndfwk_detect.vendor
+
+# Quick Tap
+TARGET_SUPPORTS_QUICK_TAP := true
 
 # RIL
 PRODUCT_PACKAGES += \
