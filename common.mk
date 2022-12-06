dbcaa45dcf4e21fe46cb08fe31fecaa85470ceae
Terminator-J <terminator_j@hotbutteredmonkey.com>
2022-12-06 01:53:30 -0800

    sdm845-common: packages: Include prebuilt OnePlus Camera
    Includes adaptations of the following commits:
    Vachounet - "set client package name"
    AnierinB - "Add OnePlus Camera support!"
    Hikari-no-Tenshi - "Address Oneplus Camera related denials"
    DennySPB - [many sepolicy edits]
    ... and a million others I'm sure.
    
    Co-authored-by: DennySPB <dennyspb@gmail.com>
    Co-authored-by: Vachounet <vachounet@live.fr>
    Co-authored-by: AnierinB <anierin@evolution-x.org>
    Co-authored-by: Hikari-no-Tenshi <kyryljan.serhij@gmail.com>
    Co-authored-by: Omkar Chandorkar <gotenksIN@aosip.dev>
    Co-authored-by: Vaisakh Murali <vaisakhmurali@gmail.com>
    Co-authored-by: Lup Gabriel <gwolf2u.website@gmail.com>
    Co-authored-by: DarkJoker360 <simoespo159@gmail.com>
    
    [again, adding commented option because it's not needed NOW,
    but I don't want to forget it's THERE in case they start playing
    with things that break OP Camera again]

--- common.mk
+++ common.mk
@@ -352,6 +352,13 @@ PRODUCT_PACKAGES += \
 PRODUCT_BOOT_JARS += \
     oneplus-fwk
 
+# OnePlus Camera
+PRODUCT_PACKAGES += \
+    vendor.oneplus.hardware.camera-V1.0-java \
+    vendor.oneplus.hardware.CameraMDMHIDL-V1.0-java
+
+$(call inherit-product-if-exists, vendor/oneplus/apps/sdm845/config.mk)
+
 # Power
 PRODUCT_PACKAGES += \
     android.hardware.power-service-qti \
