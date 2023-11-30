e2a2447b205a81893e9689000ed45eb8a740059f
Terminator-J <terminator_j@hotbutteredmonkey.com>
2023-11-29 20:05:19 -0800

    sdm845-common: Fixup! Update system WFD blobs
    Now that we have the fixed-up lib64/libwfdnative.so file pinned,
    don't re-run blob_fixup() on it during next pass of extract-utils.

--- extract-files.sh
+++ extract-files.sh
@@ -72,10 +72,6 @@ function blob_fixup() {
             [ "$2" = "" ] && return 0
             apktool_patch "${2}" "${MY_DIR}/blob-patches/PowerOffAlarm.patch" -r
             ;;
-        system_ext/lib64/libwfdnative.so)
-            [ "$2" = "" ] && return 0
-            sed -i "s/android.hidl.base@1.0.so/libhidlbase.so\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00/" "${2}"
-            ;;
         vendor/etc/init/android.hardware.biometrics.fingerprint@2.1-service.oneplus.rc)
             [ "$2" = "" ] && return 0
             sed -i "s/@2.1-service$/@2.1-service.oneplus/" "${2}"
