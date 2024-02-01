Terminator-J/crdroid_device_oneplus_sdm845-common

sdm845-common: BoardConfig: Set kernel clang options

BoardConfigCommon.mk
@@ -51,7 +51,9 @@ BOARD_KERNEL_PAGESIZE := 4096
 BOARD_KERNEL_SEPARATED_DTBO := true
 BOARD_MKBOOTIMG_ARGS += --header_version $(BOARD_BOOT_HEADER_VERSION)
 TARGET_KERNEL_ARCH := arm64
+TARGET_KERNEL_CLANG_VERSION := r487747c
 TARGET_KERNEL_CONFIG := enchilada_defconfig
+TARGET_KERNEL_LLVM_BINUTILS := true
 TARGET_KERNEL_SOURCE := kernel/oneplus/sdm845
 
 # Platform

crdroidandroid/android_device_oneplus_sdm845-common

BoardConfigCommon.mk
@@ -63,7 +63,9 @@ BOARD_KERNEL_PAGESIZE := 4096
 BOARD_KERNEL_SEPARATED_DTBO := true
 BOARD_MKBOOTIMG_ARGS += --header_version $(BOARD_BOOT_HEADER_VERSION)
 TARGET_KERNEL_ARCH := arm64
+TARGET_KERNEL_CLANG_VERSION := latest
 TARGET_KERNEL_CONFIG := enchilada_defconfig vendor/debugfs.config
+TARGET_KERNEL_LLVM_BINUTILS := true
 TARGET_KERNEL_SOURCE := kernel/oneplus/sdm845
 
 # Platform