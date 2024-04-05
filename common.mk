daece1246c0f29ffa2fd5d55a80e8fda0d38953a
Terminator-J <terminator_j@hotbutteredmonkey.com>
2024-04-04 17:56:38 -0700

    sdm845-common: features: Enable Quick Tap feature

--- common.mk
+++ common.mk
@@ -397,6 +397,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/qmi/qmi_fw.conf:$(TARGET_COPY_OUT_VENDOR)/etc/qmi_fw.conf
 
+# Quick Tap
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/quick_tap.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/quick_tap.xml
+
 # Retrofit Dynamic Partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 PRODUCT_RETROFIT_DYNAMIC_PARTITIONS := true
