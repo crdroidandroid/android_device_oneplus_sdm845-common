e68f236aa5281cdeef4137779d26438a0f35e981
Terminator-J <terminator_j@hotbutteredmonkey.com>
2024-05-19 09:21:11 -0700

    [TEMP HAX] boot permissive

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -55,6 +55,7 @@ BOARD_KERNEL_CMDLINE := \
     msm_rtb.filter=0x237 \
     service_locator.enable=1 \
     swiotlb=2048
+BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
 BOARD_KERNEL_PAGESIZE := 4096
 BOARD_KERNEL_SEPARATED_DTBO := true
